4bde0fae16dc415209e423151ee5dc7698017f10
Bejte <bestey73@gmail.com>
2026-04-06 18:09:39 +0300

    a

--- src/processor.py
+++ src/processor.py
@@ -1,12 +1,10 @@
 import json
 
 def process_user_data(json_str):
-    # BUG 1: The 'eval' risk
     data = eval(json_str) 
     
     results = []
     for item in data:
-        # BUG 2: Unhandled KeyError if 'id' is missing
         val = item['value']
         if val > 100:
             results.append(item['id'])
@@ -14,5 +12,4 @@ def process_user_data(json_str):
     return results
 
 def calculate_discount(price, discount_percent):
-    # BUG 3: Potential DivisionByZero and precision errors
     return price - (price * (discount_percent / 100))
